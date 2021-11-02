283fcee7ed8c653d49fab5ab31c8ec8c2b4092ed
J. Drwenski <j.drwenski@medienberufe.de>
2021-11-02 21:18:28 +0100

    switched to isometric

--- SimpleIsometric.js
+++ SimpleIsometric.js
@@ -1,3 +1,5 @@
+/* 	Folgende Matrix stellt das Spielfeld mit allen Inhalten dar.
+	Die Spielfigur wird anschlieﬂend separat gesetzt!	*/
 var feld = [[1,0,1,1],
 			[0,0,0,1],
 			[0,0,0,0],
@@ -6,14 +8,15 @@ var figurX = 1;
 var figurY = 2;
 feld[figurY][figurX] = 2;
 
+/* Laden der Bilddateien */
 var kachel = new Image();
 var stein = new Image();
 var figur = new Image();
-kachel.src = "green.gif";
-stein.src = "blue.gif";
-figur.src = "red.gif";
-var offsetX = 0;
-var offsetY = 0;
+kachel.src = "kachel.gif";
+stein.src = "stein.gif";
+figur.src = "figur.gif";
+var offsetX = 200;
+var offsetY = 100;
 var canvas, context;
 
 
@@ -22,26 +25,36 @@ function init() {
   context = canvas.getContext("2d");
 }
 
-
+/* 	Diese Funktion zum Zeichnen des Spielfelds muss immer wieder neu
+	aufgerufen werden, wenn sich die Matrix ‰ndert!	*/
 function zeichneFeld() 
 {
  context.clearRect(0,0,canvas.width,canvas.height);
  for (let i=0;i<feld.length;i++)
   for (let j=0;j<feld[i].length;j++) 
   {
-	let x = j*kachel.width+offsetX;
-	let y = i*kachel.height+offsetY;
+	/* 	F¸r die isometrische Ansicht werden die 2D-Koordinaten x,y 
+		auf die Diagonalen x-y und x+y mit dem Seitenverh‰ltnis 2:1
+		transformiert ... nur etwas Mathematik!	*/
+	let x = j*kachel.height;
+	let y = i*kachel.height;
+	let isoX = x-y;
+	let isoY = (x+y)/2;
+	isoX += offsetX;
+	isoY += offsetY;
 	if (feld[i][j]==0)	//normale Kachel
 	{
-	  context.drawImage(kachel,x,y,kachel.width,kachel.height);
+	  context.drawImage(kachel,isoX,isoY,kachel.width,kachel.height);
 	}
 	if (feld[i][j]==1) //Hindernis
 	{
-	  context.drawImage(stein,x,y,stein.width,stein.height);
+	  isoY -= stein.height-kachel.height;
+	  context.drawImage(stein,isoX,isoY,stein.width,stein.height);
 	}
 	if (feld[i][j]==2) //Spielfigur
 	{
-	  context.drawImage(figur,x,y,figur.width,figur.height);
+	  isoY -= figur.height-kachel.height;
+	  context.drawImage(figur,isoX,isoY,figur.width,figur.height);
 	}
   }
 } 
